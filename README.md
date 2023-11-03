faa930603f4c4fd5a8b5f18be51b600db06dce8d
Alexandre Girard Davila <git@alexgirard.com>
2023-11-03 19:09:09 +0100

    feat: python3 main.py

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # tensordock_deploy
+
 Script to deploy VM on marketplace.tensordock.com using API
+
+## install
+
+``` sh
+git clone https://github.com/alx/tensordock_deploy.git
+cd tensordock_deploy
+
+python3 -m venv .venv && source .venv/bin/activate
+pip install -r requirements.txt
+
+cp config.json.sample config.json
+cp clound_init.yml. sample cloud_init.yml
+```
+
+## config
+
+In `config.json`, replace `TENSORDOCK_API_KEY`, `TENSORDOCK_API_TOKEN`, `VM_NAME` and `VM_PASSWORD`
+
+`host_configs[priority]` level allow script to choose the nicest setup
+
+## run
+
+### start a new VM
+
+``` sh
+python3 main.py
+```
+
+### delete running VM
+
+``` sh
+python3 main.py --delete
+```
+
+### get info on running VM
+
+``` sh
+python3 main.py --info
+```
---
+++ main.py
@@ -0,0 +1,252 @@
+import os
+import sys
+from time import sleep
+import requests
+import json
+from pathlib import Path
+import logging
+from urllib.parse import urlencode
+import yaml
+
+CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
+with open(Path(CURRENT_PATH, "config.json"), "r") as f:
+    config = json.load(f)
+
+LOG_FILENAME = Path(CURRENT_PATH, config["log_filename"])
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s %(message)s",
+    filename=LOG_FILENAME,
+    level=logging.DEBUG,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logging.getLogger().addHandler(logging.StreamHandler())
+
+def is_api_available():
+    url_path = "/api/v0/auth/test"
+    response = requests.request(
+        "POST",
+        config["tensordock"]["api_url"] + url_path,
+        data = {
+            'api_key': config["tensordock"]["api_key"],
+            'api_token': config["tensordock"]["api_token"]
+        }
+    )
+    sleep(1)
+    data = json.loads(response.text)
+    return data["success"]
+
+def get_host_nodes():
+    url_path = "/api/v0/client/deploy/hostnodes"
+    response = requests.request(
+        "GET",
+        config["tensordock"]["api_url"] + url_path,
+    )
+    sleep(1)
+    return json.loads(response.text)
+
+def is_host_eligible(host):
+
+    if not host["status"]["online"] or \
+       host["specs"]["cpu"]["amount"] == 0:
+        return False
+
+    for host_config in config["host_configs"]:
+
+        logging.debug(f"Test RAM: %s" % (host_config["ram"] <= int(host["specs"]["ram"]["amount"])))
+        logging.debug(f"Test hdd: %s" % (host_config["hdd"] <= int(host["specs"]["storage"]["amount"])))
+        logging.debug(f"Test gpu model: %s" % (host_config["gpu_model"] in host["specs"]["gpu"].keys()))
+
+        if host_config["gpu_model"] in host["specs"]["gpu"].keys():
+
+            logging.debug(f"Test gpu amount: %s" % (host_config["gpu_count"] <= host["specs"]["gpu"][host_config["gpu_model"]]["amount"]))
+            logging.debug(f"Test ports: %s" % (len(host_config["internal_ports"]) <= len(host["networking"]["ports"])))
+
+            if host_config["ram"] <= host["specs"]["ram"]["amount"]                                  and \
+                host_config["hdd"] <= host["specs"]["storage"]["amount"]                             and \
+                host_config["gpu_model"] in host["specs"]["gpu"].keys()                             and \
+                host_config["gpu_count"] <= host["specs"]["gpu"][host_config["gpu_model"]]["amount"] and \
+                len(host_config["internal_ports"]) <= len(host["networking"]["ports"]):
+                return host_config
+
+    # host is not eligible
+    return False
+
+def deploy_machine(host, host_config):
+
+    url_path = "/api/v0/client/deploy/single"
+    num_ports = len(host_config["internal_ports"])
+
+    payload = {
+        'api_key': config["tensordock"]["api_key"],
+        'api_token': config["tensordock"]["api_token"],
+        'password': host_config["password"],
+        'name': host_config["name"],
+        'gpu_count': host_config["gpu_count"],
+        'gpu_model': host_config["gpu_model"],
+        'vcpus': host_config["vcpus"],
+        'ram': host_config["ram"],
+        'external_ports': str(set(host["networking"]["ports"][:num_ports])),
+        'internal_ports': str(set(host_config["internal_ports"])),
+        'hostnode': host["id"],
+        'storage': host_config["hdd"],
+        'operating_system': host_config["os"]
+    }
+
+    if "cloudinit_file" in host_config and \
+        os.path.isfile(host_config["cloudinit_file"]):
+        with open(host_config["cloudinit_file"], 'r') as f:
+            payload["cloudinit_script"] = f.read().replace('\n', r'\n')
+
+    logging.debug("Deploying machine")
+    logging.debug(host)
+    logging.debug("---")
+    logging.debug(payload)
+    logging.debug("===")
+
+    req = requests.Request(
+        "POST",
+        config["tensordock"]["api_url"] + url_path,
+        headers = {},
+        data = payload
+    )
+    prepared = req.prepare()
+
+    def pretty_print_POST(req):
+        """
+        At this point it is completely built and ready
+        to be fired; it is "prepared".
+
+        However pay attention at the formatting used in
+        this function because it is programmed to be pretty
+        printed and may differ from the actual request.
+        """
+        print('{}\n{}\r\n{}\r\n\r\n{}'.format(
+            '-----------START-----------',
+            req.method + ' ' + req.url,
+            '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
+            req.body,
+        ))
+
+    pretty_print_POST(prepared)
+
+    response = requests.Session().send(prepared)
+    logging.debug(response.text)
+    data = json.loads(response.text)
+    success = data["success"]
+    sleep(1)
+
+    if success:
+        logging.info("Machine deployed")
+        ssh_port = 0
+        http_port = 0
+        for port in data["port_forwards"]:
+
+            if int(data["port_forwards"][port]) == 22:
+                ssh_port = int(port)
+                logging.info(f"ssh-keygen -f $HOME/.ssh/known_hosts -R '[%s]:%i'" % (data["ip"], ssh_port))
+                logging.info(f"ssh -o StrictHostKeyChecking=accept-new -p %i user@%s" % (ssh_port, data["ip"]))
+
+            if int(data["port_forwards"][port]) in [8888, 5000]:
+                http_port = int(port)
+                logging.info(f"http://%s:%i" % (data["ip"], http_port))
+
+    return success
+
+def info_deploys():
+
+    url_path = "/api/v0/client/list"
+    response = requests.request(
+        "POST",
+        config["tensordock"]["api_url"] + url_path,
+        data = {
+            'api_key': config["tensordock"]["api_key"],
+            'api_token': config["tensordock"]["api_token"]
+        }
+    )
+    sleep(1)
+    response = json.loads(response.text)
+
+    for server_uuid in response["virtualmachines"]:
+
+        url_path = "/api/v0/client/get/single"
+        response = requests.request(
+            "POST",
+            config["tensordock"]["api_url"] + url_path,
+            data = {
+                'api_key': config["tensordock"]["api_key"],
+                'api_token': config["tensordock"]["api_token"],
+                'server': server_uuid
+            }
+        )
+        response = json.loads(response.text)
+        logging.debug(response)
+        sleep(1)
+
+def delete_deploys():
+
+    url_path = "/api/v0/client/list"
+    response = requests.request(
+        "POST",
+        config["tensordock"]["api_url"] + url_path,
+        data = {
+            'api_key': config["tensordock"]["api_key"],
+            'api_token': config["tensordock"]["api_token"]
+        }
+    )
+    sleep(1)
+    response = json.loads(response.text)
+
+    for server_uuid in response["virtualmachines"]:
+
+        url_path = "/api/v0/client/delete/single"
+        response = requests.request(
+            "POST",
+            config["tensordock"]["api_url"] + url_path,
+            data = {
+                'api_key': config["tensordock"]["api_key"],
+                'api_token': config["tensordock"]["api_token"],
+                'server': server_uuid
+            }
+        )
+        sleep(1)
+
+def deploy_node():
+    hosts = get_host_nodes()
+    host_nodes_keys = hosts["hostnodes"].keys()
+
+    eligible_hosts = []
+
+    for key in host_nodes_keys:
+        host = hosts["hostnodes"][key]
+        logging.debug(host)
+        host_config = is_host_eligible(host)
+        if host_config:
+            host["id"] = key
+            eligible_hosts.append([host, host_config])
+
+    if len(eligible_hosts) == 0:
+        logging.info("No eligible hosts found")
+        return False
+
+    elif len(eligible_hosts) > 1:
+        logging.info("Multiple eligible hosts found")
+        eligible_hosts = sorted(
+            eligible_hosts,
+            key=lambda host, host_config: host_config['priority'],
+            reverse=True
+        )
+
+    host, host_config = eligible_hosts[0]
+    success = deploy_machine(host, host_config)
+
+if is_api_available():
+
+    if "--delete" in sys.argv:
+        delete_deploys()
+    elif "--info" in sys.argv:
+        info_deploys()
+    else:
+        deploy_node()
+
+else:
+    logging.info("API not available")
